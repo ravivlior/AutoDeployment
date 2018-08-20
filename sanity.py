13b1c16264a74e7dd18de29de78dd3e2b63470e9
ravivlior <ravivlior@gmail.com>
2018-08-20 08:56:52 +0300

    add xml functions

--- sanity.py
+++ sanity.py
@@ -1,23 +1,31 @@
 import shutil
 import xml.etree.ElementTree
 
-# copy files
+## copy files
 # src = "/home/ravivl/PycharmProjects/db/1/data.txt"
 # dst = "/home/ravivl/PycharmProjects/db/2"
 # shutil.copy2(src,dst)
-#
-# copy folder
+
+## copy folder
 # src = "/home/ravivl/PycharmProjects/db/1/dataFolder"
 # dst = "/home/ravivl/PycharmProjects/db/2/newDataFolder"
 # shutil.copytree(src,dst)
 
-# update XML file
-xmlFile = xml.etree.ElementTree.parse('/home/ravivl/PycharmProjects/db/1/file.xml')
-new_tag = xml.etree.ElementTree.SubElement(xmlFile.getroot(), 'itemXXX')
-new_tag.text = 'body text'
-new_tag.attrib['x'] = '1' # must be str; cannot be an int
-new_tag.attrib['y'] = 'abc'
+## update XML element text
+# xmlFile = xml.etree.ElementTree.parse('/home/ravivl/PycharmProjects/db/1/file.xml')
+# xmlFile.find('.//item[2]/price').text = '111'
+
+## update XML element attribute
+# xmlFile = xml.etree.ElementTree.parse('/home/ravivl/PycharmProjects/db/1/file.xml')
+# sh = xmlFile.find('.//item[2]/price')
+# sh.set('attributeName', 'value')
+
+## add Element to XML
+# new_tag = xml.etree.ElementTree.SubElement('.//item[2]/price', 'newElement')
+# new_tag.text = 'body text'
+# new_tag.attrib['x'] = '1' # must be str; cannot be an int
+# new_tag.attrib['y'] = 'abc'
 
 # Write back to file
-#et.write('file.xml')
+# et.write('file.xml')
 xmlFile.write('/home/ravivl/PycharmProjects/db/1/file_new.xml')
